2691aa75ad61093be781ed6027dc1f60c15e9656
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2021-08-19 23:27:36 +0600

    service two section done

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,36 +1,90 @@
-import React from 'react';
+import React, { useState } from "react";
+import howToUseApp from "./api/howtouse";
 
 const AboutUs = () => {
-    return (
-        <div>
-            <section className="commonSection ourServices">
-                <div className="container mb-5">
-                    <div className="row">
-                        <div className="col-12 col-lg-5 text-center serviceLeftSideImg">
-                            <img src="./images/hero3.jpg" alt="not found" className="img-fluid"/>
-                        </div>
-                        <div className="col-12 col-lg-7 ourServiceList">
-                            <h3 className="miniTitle">
-                                -- AVAILABLE @ GOOGLE AND IOS APP STORE ONLY
-                            </h3>
-                            <h1 className="mainHeading">How to use the App?</h1>
-                            <div className="row ourServicesInfo">
-                                <div className="col-1 ourServicesNumber">1</div>
-                                <div className="col-10 ourServicesData">
-                                    <h2 className="">Sign in</h2>
-                                    <p className="main_hero_para">
-                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque numquam odit excepturi similique iure sint minus eveniet. Ab repellat consequatur, excepturi hic, possimus labore dolore eligendi asperiores nostrum ipsam delectus.
-                                    </p>
-                                </div>
-                            </div>
-                            <br/>
-                            <button className="btn_style btn_style_border">learn more</button>
-                        </div>
+  const [aboutData, setAboutDAta] = useState(howToUseApp);
+  return (
+    <div>
+      <section className="commonSection ourServices">
+        <div className="container mb-5">
+          <div className="row">
+            <div className="col-12 col-lg-5 text-center serviceLeftSideImg">
+              <img
+                src="./images/hero3.jpg"
+                alt="not found"
+                className="img-fluid"
+              />
+            </div>
+            <div className="col-12 col-lg-7 ourServiceList">
+              <h3 className="miniTitle">
+                -- AVAILABLE @ GOOGLE AND IOS APP STORE ONLY
+              </h3>
+              <h1 className="mainHeading">How to use the App?</h1>
+
+              {aboutData.map((curr) => {
+                const { id, title, info } = curr;
+                return (
+                  <>
+                    <div className="row ourServicesInfo" key={id}>
+                      <div className="col-1 ourServicesNumber">{id}</div>
+                      <div className="col-10 ourServicesData">
+                        <h2 className="">{title}</h2>
+                        <p className="main_hero_para">{info}</p>
+                      </div>
                     </div>
-                </div>
-            </section>
+                  </>
+                );
+              })}
+
+              <br />
+              <button className="btn_style btn_style_border">learn more</button>
+            </div>
+          </div>
         </div>
-    );
+      </section>
+
+      <section className="commonSection ourServices ourServicesRightSide">
+        <div className="container mb-5">
+          <div className="row">
+            <div className="col-12 col-lg-7 ourServicesRightSideContent d-flex justify-content-center align-items-start flex-column">
+              <h3 className="miniTitle">
+                -- SUPPORT IN ANY LANGUAGES
+              </h3>
+              <h1 className="mainHeading">
+                  World class support is <br/> available 24/7
+              </h1>
+
+              {aboutData.map((curr) => {
+                const { id, title, info } = curr;
+                return (
+                  <>
+                    <div className="row ourServicesInfo" key={id}>
+                      <div className="col-1 ourServicesNumber">{id}</div>
+                      <div className="col-10 ourServicesData">
+                        <h2 className="">{title}</h2>
+                        <p className="main_hero_para">{info}</p>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+
+              <br />
+              <button className="btn_style btn_style_border">learn more</button>
+            </div>
+
+            <div className="col-12 col-lg-5 serviceRightSideImg">
+              <img
+                src="./images/callcenter.jpg"
+                alt="not found"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default AboutUs;
